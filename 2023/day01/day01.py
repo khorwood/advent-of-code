7e46da59afdf39a03009a16263fab65ccc8410cb
Kristian Horwood <khorwood@gmail.com>
2023-12-01 22:19:41 -0500

    Cleanup 2023 - Day 1 and add gitignore

--- 2023/day01/day01.py
+++ 2023/day01/day01.py
@@ -2,16 +2,13 @@ import re
 
 def part1():
     regex = re.compile('\d')
-
     sum = 0
     with open('input', 'r') as f:
         for line in f:
             numbers = regex.findall(line)
-            num = int(numbers[0])
-            if len(numbers) == 1:
-                sum += num * 10 + num
-            else:
-                sum += num * 10 + int(numbers[-1])
+            first = int(numbers[0])
+            last = int(numbers[-1])
+            sum += first * 10 + last
     print(sum)
 
 NUM_MAP = { 'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9 }
@@ -23,13 +20,12 @@ def parseNum(input):
 
 def part2():
     regex = re.compile('(?=(\d)|(one)|(two)|(three)|(four)|(five)|(six)|(seven)|(eight)|(nine))')
-
     sum = 0
     with open('input','r') as f:
         for line in f:
             numbers = regex.findall(line)
-            first = parseNum([i for i in numbers[0] if i != ''][0])
-            last = parseNum([i for i in numbers[-1] if i != ''][0])
+            first = parseNum(next(i for i in numbers[0] if i != ''))
+            last = parseNum(next(i for i in numbers[-1] if i != ''))
             sum += first * 10 + last
     print(sum)
 
